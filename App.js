2677e59a92cb80e4d7378daa335726e248c31317
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-06-03 21:50:15 +0200

    Initial Project Setup Installed NativeWind and TrailWind for better CSS Intalled basic dependecies

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={{marginTop: 40}} className="bg-red-400">
+      <Text>Hello World Pavan</Text>
+    </View>
+  );
+}
+export default App;
