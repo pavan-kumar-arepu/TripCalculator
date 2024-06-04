140ed68fb1603a908337e5d9b9bdf8f8b1349cb1
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-06-04 07:59:21 +0200

    1. Designed Home page with Top Title, Logout button and Landing Highlight image. 2. Added required assets

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,25 @@
-import {View, Text} from 'react-native';
+import {View, Text, Touchable, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import ScreenWrapper from '../components/screenWrapper';
+import {colors} from '../theme';
 
 export default function HomeScreen() {
   return (
-    <ScreenWrapper>
-      <Text>HomeScreen</Text>
+    <ScreenWrapper className="flex-1">
+      <View className="flex-row justify-between items-center p-4">
+        <Text className={`${colors.heading} font-bold text-3xl shadow-sm`}>
+          Trip Tracker
+        </Text>
+        <TouchableOpacity className="p-2 px-3 bg-white border border-gray-200 roundded-full">
+          <Text className={colors.heading}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex-row justify-center items-center bg-blue-200 rounded-xl mx-4 mb-4">
+        <Image
+          source={require('../assets/images/banner.png')}
+          className="w-60 h-60"
+        />
+      </View>
     </ScreenWrapper>
   );
 }
---
+++ theme/index.js
@@ -0,0 +1,4 @@
+export const colors = {
+  heading: 'text-gray-600',
+  button: '#50c878',
+};
